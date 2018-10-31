e32abef641d9e77d8d39f6b7fd32ba84526d8f7b
Felix Büttner <35602050+bttner@users.noreply.github.com>
2018-10-31 23:12:07 +0100

    update log

--- kanelbulle/utils/log.py
+++ kanelbulle/utils/log.py
@@ -5,7 +5,8 @@ from kanelbulle.config import config
 
 # Log format to use.
 FORMATTER = logging.Formatter('%(asctime)s | %(name)-10s | '
-                              '%(levelname)-8s | %(message)s', '%Y-%m-%d %H:%M')
+                              '%(levelname)-8s | %(message)s', '%Y-%m-%d '
+                                                               '%H:%M:%S')
 
 # Log levels to use.
 LOG_LEVELS = {
@@ -14,6 +15,15 @@ LOG_LEVELS = {
     'WARNING': logging.WARNING,
     'INFO': logging.INFO,
     'DEBUG': logging.DEBUG,
+}
+
+# Associate each log level with a HTML color.
+LOG_LEVEL_COLOR = {
+    'CRITICAL': "purple",
+    'ERROR': "red",
+    'WARNING': "blue",
+    'INFO': "black",
+    'DEBUG': "green",
 }
 
 # Load configurations from file. If it fails, take predefined values.
@@ -73,3 +83,4 @@ def get_logger(name):
 app = get_logger('app')
 config = get_logger('config')
 log = get_logger('log')
+gui = get_logger('gui')
